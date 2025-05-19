8e7c82545ed6bb90ad884053121062380fbfe94c
Jopepe18 <pepelidouioanna@gmail.com>
2025-05-19 19:12:14 +0200

    ips

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,8 +2,8 @@ variable "ssh_allowed_ips" {
   type    = list(string)
   default = [
     "79.103.26.106/32",
-    "45.139.214.104/32",
-    "141.255.126.50/32"
+    "45.139.214.56/32",
+    "141.237.236.10/32"
   ]
   description = "List of IPs allowed to SSH into EC2"
 }
